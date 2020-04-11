52f9fa7b1ba8f36612aaccb405cf8d493644cb83
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-11 14:01:25 +0200

    Update

--- server/index.js
+++ server/index.js
@@ -306,6 +306,91 @@ server.post("/search", async (req, res) => {
     res.json({ error: error });
   }
 });
+
+////////////////////////////////////////////////////////////////// UTILS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+////SAVE SONG FUNCTION
+async function saveSong(req, res, next) {
+  const userId = isAuth(req);
+  if (userId !== null) {
+    try {
+      //Check If Book is Already Saved
+      const checkDB = await pool.query(
+        `SELECT * FROM music WHERE song_key = '${req.body.song_key}'
+             AND person_id = '${userId}'`
+      );
+
+      const doesSongExist = checkDB.rows[0];
+
+      if (doesSongExist !== undefined)
+        throw new Error(`" ${req.body.song_title} " is Already Saved.`);
+
+      const values = [
+        userId,
+        req.body.song_key,
+        req.body.artist_image,
+        req.body.artist_name,
+        req.body.song_title,
+        req.body.album_title,
+        req.body.explicit_lyrics,
+        req.body.song,
+      ];
+
+      pool.query(
+        `INSERT INTO music (person_id, song_key, artist_image, artist_name,
+          song_title, album_title, explicit_lyrics, song
+          ) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8)`,
+        values,
+        (q_err, q_res) => {
+          if (q_err) return next(q_err);
+          res.json({ key: req.body.song_key, message: "Song Saved" });
+        }
+      );
+    } catch (err) {
+      res.json({ key: req.body.song_key, message: err.message });
+    }
+  }
+}
+
+server.post("/search/saveSong", async (req, res, next) => {
+  saveSong(req, res, next);
+});
+
+///FETCHING SAVED Songs
+server.get("/MySongs", async (req, res) => {
+  try {
+    const userId = isAuth(req);
+    if (userId !== null) {
+      const songs = await pool.query(
+        `SELECT * FROM music WHERE person_id = '${userId}'`
+      );
+      res.send({
+        data: songs.rows,
+      });
+    }
+  } catch (err) {
+    res.json({
+      error: `${err.message}`,
+    });
+  }
+});
+
+//DELETING SAVED SONGS
+server.post("/MySongs/Delete", async (req, res) => {
+  try {
+    const userId = isAuth(req);
+    if (userId !== null) {
+      await pool.query(
+        `DELETE FROM music WHERE song_key = '${req.body.song_key}'
+           AND person_id = '${userId}'`
+      );
+
+      res.json({ message: `${req.body.movie_title} Deleted.` });
+    }
+  } catch (error) {
+    res.json({ error: "Movie Not Deleted." });
+  }
+});
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 server.listen(process.env.PORT, () =>
