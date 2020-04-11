fe9e5dfcdba528526ab3e21ca3e3564d3ab5123b
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-11 07:25:08 +0200

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Router, navigate } from "@reach/router";
 import Header from "./components/Header/Header.js";
+import Search from "./components/Search/Search.js";
 import "./App.css";
 
+export const UserContext = React.createContext([]);
+
 function App() {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const logOutCallback = async () => {
+    await fetch("http://localhost:4020/logout", {
+      method: "POST",
+      credentials: "include", // Needed to include the cookie
+    });
+    // Clear user from context
+    setUser({});
+    navigate("/");
+  };
+
+  // First thing, check if a refreshtoken exist(If USer STill Has Access)
+  useEffect(() => {
+    async function checkRefreshToken() {
+      const result = await (
+        await fetch("http://localhost:4020/refresh_token", {
+          method: "POST",
+          credentials: "include", // Needed to include the cookie
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+      ).json();
+      setUser({
+        accesstoken: result.accesstoken,
+      });
+      setLoading(false);
+    }
+    checkRefreshToken();
+  }, []);
+
   return (
     <div
       style={{
@@ -11,19 +48,33 @@ function App() {
         margin: "0 auto",
       }}
     >
-      <h2
-        style={{
-          fontSize: "1.8rem",
-          fontWeight: "bolder",
-          textAlign: "center",
-          color: "white",
-        }}
-      >
-        CURRENT TOP THREE ARTISTS
-        <hr style={{ width: "22%", margin: "0 auto" }} />
-      </h2>
-      <br />
-      <Header />
+      {loading ? (
+        <div
+          className="spinner-grow text-dark"
+          role="status"
+          style={{ margin: "auto" }}
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <React.Fragment>
+          <Search />
+          <br />
+          <h2
+            style={{
+              fontSize: "1.8rem",
+              fontWeight: "bolder",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            CURRENT TOP THREE ARTISTS
+            <hr style={{ width: "22%", margin: "0 auto" }} />
+          </h2>
+          <br />
+          <Header />
+        </React.Fragment>
+      )}
     </div>
   );
 }
---
+++ client/src/components/Search/DummyData.js
@@ -0,0 +1,53 @@
+const initialState = {
+  DummyData: true,
+  data: [
+    {
+      id: 350171311,
+      readable: true,
+      title: "HUMBLE.",
+      title_short: "HUMBLE.",
+      title_version: "",
+      link: "https://www.deezer.com/track/350171311",
+      duration: 177,
+      rank: 881294,
+      explicit_lyrics: true,
+      explicit_content_lyrics: 1,
+      explicit_content_cover: 1,
+      preview:
+        "https://cdns-preview-e.dzcdn.net/stream/c-e99276844e12072cd449b6728f74c65f-4.mp3",
+      artist: {
+        id: 525046,
+        name: "Kendrick Lamar",
+        link: "https://www.deezer.com/artist/525046",
+        picture: "https://api.deezer.com/artist/525046/image",
+        picture_small:
+          "https://e-cdns-images.dzcdn.net/images/artist/b0f016efccad94e838c6bff3fe758db6/56x56-000000-80-0-0.jpg",
+        picture_medium:
+          "https://e-cdns-images.dzcdn.net/images/artist/b0f016efccad94e838c6bff3fe758db6/250x250-000000-80-0-0.jpg",
+        picture_big:
+          "https://e-cdns-images.dzcdn.net/images/artist/b0f016efccad94e838c6bff3fe758db6/500x500-000000-80-0-0.jpg",
+        picture_xl:
+          "https://e-cdns-images.dzcdn.net/images/artist/b0f016efccad94e838c6bff3fe758db6/1000x1000-000000-80-0-0.jpg",
+        tracklist: "https://api.deezer.com/artist/525046/top?limit=50",
+        type: "artist",
+      },
+      album: {
+        id: 39949511,
+        title: "DAMN.",
+        cover: "https://api.deezer.com/album/39949511/image",
+        cover_small:
+          "https://e-cdns-images.dzcdn.net/images/cover/7ce6b8452fae425557067db6e6a1cad5/56x56-000000-80-0-0.jpg",
+        cover_medium:
+          "https://e-cdns-images.dzcdn.net/images/cover/7ce6b8452fae425557067db6e6a1cad5/250x250-000000-80-0-0.jpg",
+        cover_big:
+          "https://e-cdns-images.dzcdn.net/images/cover/7ce6b8452fae425557067db6e6a1cad5/500x500-000000-80-0-0.jpg",
+        cover_xl:
+          "https://e-cdns-images.dzcdn.net/images/cover/7ce6b8452fae425557067db6e6a1cad5/1000x1000-000000-80-0-0.jpg",
+        tracklist: "https://api.deezer.com/album/39949511/tracks",
+        type: "album",
+      },
+    },
+  ],
+};
+
+export default initialState;
