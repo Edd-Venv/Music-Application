10be91dfe657508436fc782f4111f0d304b06abf
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-11 14:01:39 +0200

    Update

---
+++ client/src/nav/Register/Register.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useRef } from "react";
+import { navigate } from "@reach/router";
+import Navigation from "../Navigation/Navigation";
+import "./Register.css";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [state, setState] = useState({ message: "" });
+  const userNameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  useEffect(() => {
+    userNameRef.current.focus();
+  }, []);
+
+  const firstKeyDown = (event) => {
+    if (event.key === "Enter") passwordRef.current.focus();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await (
+      await fetch("http://localhost:4020/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          person_name: name,
+          password: password,
+        }),
+      })
+    ).json();
+    if (!result.error) {
+      navigate("/login");
+    } else {
+      setState({ message: result.error });
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.currentTarget.name === "name") {
+      setName(e.currentTarget.value.toUpperCase());
+    } else {
+      setPassword(e.currentTarget.value);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Navigation displayLogin={"dontDisplayLoginForm"} />
+      <form className="card mb-3" onSubmit={handleSubmit} id="register-form">
+        <h3 style={{ textAlign: "center" }}>
+          REGISTER
+          <hr />
+        </h3>
+        <div className="form-group">
+          <label htmlFor="name">USER NAME</label>
+          <input
+            className="form-control"
+            value={name}
+            onChange={handleChange}
+            type="text"
+            name="name"
+            placeholder="User Name"
+            required
+            onKeyDown={firstKeyDown}
+            ref={userNameRef}
+          />
+          {state.message}
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">PASSWORD</label>
+          <input
+            className="form-control"
+            value={password}
+            onChange={handleChange}
+            type="password"
+            name="password"
+            autoComplete="current-password"
+            placeholder="Password"
+            required
+            ref={passwordRef}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          REGISTER
+        </button>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default Register;
---
+++ client/src/nav/Settings/ChangeUserPwd.js
@@ -0,0 +1,73 @@
+import React, { useContext, useState } from "react";
+import { UserContext } from "../../App.js";
+import "./Settings.css";
+
+function ChangeUserPwd(props) {
+  const [user] = useContext(UserContext);
+  const [oldPwd, setOldPwd] = useState("");
+  const [newPwd, setNewPwd] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const result = await (
+      await fetch("http://localhost:4020/settings/changePassword", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${user.accesstoken}`,
+        },
+        body: JSON.stringify({
+          old_password: oldPwd,
+          new_password: newPwd,
+        }),
+      })
+    ).json();
+    if (!result.error) {
+      console.log(result.message);
+    } else {
+      console.log(result.error);
+    }
+
+    props.logOutCallback();
+  };
+
+  const handleInput = (event) => {
+    if (event.target.name === "old password") {
+      setOldPwd(event.target.value);
+    } else setNewPwd(event.target.value);
+  };
+  return (
+    <form className="card mb-3 form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <h3 style={{ textAlign: "center", color: "white" }}>CHANGE PASSWORD</h3>
+        <label htmlFor="old password">OLD PASSWORD</label>
+        <input
+          className="form-control"
+          value={oldPwd}
+          placeholder="Old Password"
+          type="password"
+          onChange={handleInput}
+          name="old password"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="new password">NEW PASSWORD</label>
+        <input
+          className="form-control"
+          name="new password"
+          value={newPwd}
+          placeholder="New Password"
+          type="password"
+          onChange={handleInput}
+          required
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        SUBMIT
+      </button>
+    </form>
+  );
+}
+
+export default ChangeUserPwd;
