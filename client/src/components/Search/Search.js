b27803d4c5b7b46e2c919ed45834533c68ed5c3e
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-15 15:35:35 +0200

    Update

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -4,13 +4,25 @@ import SearchForm from "./SearchForm.js";
 import SearchResult from "./SearchResult.js";
 
 function Search() {
-  const [state, setState] = useState(initialState);
-  const handleClose = () => {
+  const [state, setState] = useState({ ...initialState });
+  const handleResultsCloseButton = () => {
     setState({ ...initialState });
     document.getElementById("search-results-model").style.display = "none";
   };
 
-  const onAddSearch = (text) => {
+  const handleMusicVideoPlayButton = () => {
+    document.getElementById("search-results-music-video-model").style.display =
+      "block";
+    document.getElementById("search-results-model").style.zIndex = 0;
+  };
+
+  const handleMusicVideoCloseButton = () => {
+    document.getElementById("search-results-music-video-model").style.display =
+      "none";
+    document.getElementById("search-results-model").style.zIndex = 1;
+  };
+
+  const handleSearch = (text) => {
     (async function fetchData() {
       await fetch("http://localhost:4020/search", {
         method: "POST",
@@ -25,15 +37,20 @@ function Search() {
           return result.json();
         })
         .then((Data) => {
-          setState({ data: Data.data });
+          setState({ ...Data });
         });
     })();
   };
 
   return (
     <React.Fragment>
-      <SearchForm onAddSearch={onAddSearch} />
-      <SearchResult results={state.data} handleClose={handleClose} />
+      <SearchForm onAddSearch={handleSearch} />
+      <SearchResult
+        results={state}
+        handleClose={handleResultsCloseButton}
+        handleMusicVideoPlayButton={handleMusicVideoPlayButton}
+        handleMusicVideoCloseButton={handleMusicVideoCloseButton}
+      />
     </React.Fragment>
   );
 }
