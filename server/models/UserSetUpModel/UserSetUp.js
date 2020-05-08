60d4241aa19b70c5b3c1cf51ee3189f68176051c
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-08 11:29:54 +0200

    MCV / REST

---
+++ server/models/UserSetUpModel/UserSetUp.js
@@ -0,0 +1,95 @@
+/* eslint-disable camelcase */
+const { hash, compare } = require("bcryptjs");
+const uuid_generate = require("uuid");
+const pool = require("../../database-connection/db.js");
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendRefreshToken,
+  sendAccessToken,
+} = require("../../src/tokens.js");
+
+exports.registerModel = async (req) => {
+  const { person_name, password } = req.body;
+
+  // 1. Check if the user exist
+  const checkDB = await pool.query(
+    `SELECT person_name FROM person WHERE person_name = '${person_name}'`
+  );
+  const doesUserExist = checkDB.rows[0];
+
+  if (doesUserExist !== undefined)
+    throw new Error(`USER NAME " ${person_name} " IS TAKEN.`);
+
+  // 2. If not user exist already, hash the password
+  const hashedPassword = await hash(password, 10);
+
+  //3. Check if an image is attached then store to DB
+  if (req.file) {
+    const imageName = req.file.filename;
+
+    //Insert the user in database
+    await pool.query(
+      "INSERT INTO person (id_uid, person_name, password, person_image) VALUES($1, $2, $3, $4)",
+      [uuid_generate.v4(), person_name.toUpperCase(), hashedPassword, imageName]
+    );
+  } else if (!req.file) {
+    //Insert the user in database with default image
+    await pool.query(
+      "INSERT INTO person (id_uid, person_name, password, person_image) VALUES($1, $2, $3, $4)",
+      [
+        uuid_generate.v4(),
+        person_name.toUpperCase(),
+        hashedPassword,
+        "default.jpeg",
+      ]
+    );
+    throw new Error("File Not found..", req.file);
+  }
+};
+
+exports.loginModel = async (req, res) => {
+  const { person_name, password } = req.body;
+
+  // 1. Check if the user exists
+  const checkDB = await pool.query(
+    `SELECT * from person where person_name = '${person_name}'`
+  );
+  const doesUserExist = checkDB.rows[0];
+
+  if (!doesUserExist) throw new Error(`" ${person_name} " IS AN INVALID USER.`);
+
+  // 2. Compare crypted password and see if it checks out. Send error if not
+  const user = doesUserExist;
+
+  const valid = await compare(password, user.password);
+  if (!valid) throw new Error("Password not correct");
+
+  // 3. Create Refresh- and Accesstoken
+  const accesstoken = createAccessToken(user.id_uid);
+  const refreshtoken = createRefreshToken(user.id_uid);
+
+  // 4. Store Refreshtoken with user in db
+  await pool.query(
+    `UPDATE person SET refreshtoken = '${refreshtoken}'
+           WHERE id_uid = '${user.id_uid}'`
+  );
+
+  const userImage = await pool.query(
+    `SELECT person_image FROM person WHERE id_uid = '${user.id_uid}'`
+  );
+
+  // 5. Send token. Refreshtoken as a cookie and accesstoken as a regular response
+  sendRefreshToken(res, refreshtoken);
+  sendAccessToken(res, req, accesstoken, userImage.rows[0].person_image);
+};
+
+exports.logOutModel = async (req, res) => {
+  res.clearCookie("refreshtoken", { path: "/refresh_token" });
+
+  // Logic here to also remove refreshtoken from dataBase
+  const { person_name } = req.body;
+  await pool.query(
+    `UPDATE person SET refreshtoken = '' WHERE person_name = '${person_name}'`
+  );
+};
--- server/routes/ApiCalls/ApiCalls.js
+++ server/routes/ApiCalls/ApiCalls.js
@@ -1,39 +1,8 @@
-const NodeCache = require("node-cache");
-const fetch = require("node-fetch");
-const Cache = new NodeCache();
 const express = require("express");
+const apiController = require("../../controllers/MusicControllers/ApiCalls/ApiCalls.js");
+
 const router = express.Router();
 
-/////Fetching Header Data/ Charts
-router.get("/ChartData", async (req, res) => {
-  const chartsApi = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0`;
-
-  try {
-    const exists = Cache.has("ApiData");
-
-    if (exists) {
-      res.json({ ...Cache.get("ApiData") });
-    } else {
-      const charts = await (
-        await fetch(chartsApi, {
-          headers: { origin: "https://cors-anywhere.herokuapp.com/" },
-        })
-      ).json();
-
-      const finalResult = {
-        ChartData: {
-          tracks: charts.tracks.data.slice(0, 8),
-          albums: charts.albums.data.slice(0, 6),
-          artists: charts.artists.data.slice(0, 7),
-        },
-      };
-
-      Cache.set("ApiData", { ...finalResult }, 691200);
-      res.json({ ...finalResult });
-    }
-  } catch (error) {
-    res.json({ error: error });
-  }
-});
+router.get("/ChartData", apiController.getChartData);
 
 module.exports = router;
