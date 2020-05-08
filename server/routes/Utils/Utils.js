ecae71535c1d61a0f4c6b5ca4375981cc6d8d2e3
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-08 11:29:19 +0200

    Update

--- server/routes/Utils/Utils.js
+++ server/routes/Utils/Utils.js
@@ -1,103 +1,20 @@
-const pool = require("../../database-connection/db.js");
-const { isAuth } = require("../../src/isAuth.js");
+/* eslint-disable camelcase */
 const express = require("express");
+const musicUtilsController = require("../../controllers/MusicControllers/Utils/Utils.js");
+
 const router = express.Router();
 
-///SAVE SONG FUNCTION
-async function saveSong(req, res, next) {
-  const userId = isAuth(req);
-  if (userId !== null) {
-    try {
-      //Check If Book is Already Saved
-      const checkDB = await pool.query(
-        `SELECT * FROM music WHERE song_key = '${req.body.song_key}'
-               AND person_id = '${userId}'`
-      );
+//router.post("/search/saveSong", musicUtilsController.saveSong);
 
-      const doesSongExist = checkDB.rows[0];
-
-      if (doesSongExist !== undefined)
-        throw new Error(`" ${req.body.song_title} " is Already Saved.`);
-
-      const values = [
-        userId,
-        req.body.song_key,
-        req.body.artist_image,
-        req.body.artist_name,
-        req.body.song_title,
-        req.body.album_title,
-        req.body.explicit_lyrics,
-        req.body.song,
-      ];
-
-      pool.query(
-        `INSERT INTO music (person_id, song_key, artist_image, artist_name,
-            song_title, album_title, explicit_lyrics, song
-            ) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8)`,
-        values,
-        (q_err, q_res) => {
-          if (q_err) return next(q_err);
-          res.json({ key: req.body.song_key, message: "Song Saved" });
-        }
-      );
-    } catch (err) {
-      res.json({ key: req.body.song_key, message: err.message });
-    }
-  }
-}
-
-router.post("/search/saveSong", async (req, res, next) => {
-  saveSong(req, res, next);
-});
-
-router.post("/saveSong", async (req, res, next) => {
-  saveSong(req, res, next);
-});
+router.post("/song", musicUtilsController.saveSong);
 
 ///FETCHING SAVED SONGS
-router.get("/MySongs", async (req, res) => {
-  try {
-    const userId = isAuth(req);
-    if (userId !== null) {
-      const songs = await pool.query(
-        `SELECT * FROM music WHERE person_id = '${userId}'`
-      );
-      res.send({
-        data: songs.rows,
-      });
-    }
-  } catch (err) {
-    res.redirect("http://18.222.115.53:4020/login");
-  }
-});
+router.get("/MySongs", musicUtilsController.getMySongs);
 
 //DELETING SAVED SONGS
-router.post("/MySongs/Delete", async (req, res) => {
-  try {
-    const userId = isAuth(req);
-    if (userId !== null) {
-      await pool.query(
-        `DELETE FROM music WHERE song_key = '${req.body.song_key}'
-             AND person_id = '${userId}'`
-      );
-
-      res.json({ message: `${req.body.movie_title} Deleted.` });
-    }
-  } catch (error) {
-    res.json({ error: "Movie Not Deleted." });
-  }
-});
+router.delete("/song", musicUtilsController.deleteSong);
 
 /// SENDING SELETED BUTTON KEY
-router.post("/buttonUI", async (req, res) => {
-  try {
-    res.json({
-      key: req.body.song_key,
-      musicAudioButtonClicked: req.body.music_audio_button_click,
-    });
-  } catch (error) {
-    res.json({ error: error });
-  }
-});
+router.post("/buttonUI", musicUtilsController.handleButtonUI);
 
 module.exports = router;
