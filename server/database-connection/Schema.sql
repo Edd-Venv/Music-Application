8ba7eaf25586ce29e18528088cb9e8b2aec3d251
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-24 13:12:56 +0200

    Git Ready

--- server/database-connection/Schema.sql
+++ server/database-connection/Schema.sql
@@ -1,17 +1,4 @@
-create table show (
-    show_id SERIAL PRIMARY KEY,
-    person_id UUID NOT NULL,
-    show_image VARCHAR NOT NULL,
-    show_key BIGINT NOT NULL,
-    show_title VARCHAR NOT NULL,
-    show_runtime BIGINT NOT NULL,
-    show_status VARCHAR NOT NULL,
-    show_premiered VARCHAR NOT NULL,
-    show_genre VARCHAR NOT NULL,
-    show_rating NUMERIC,
-    show_summary VARCHAR NOT NULL,
-    id_uid UUID REFERENCES person
-);
+
 
 
 create table music (
@@ -28,33 +15,6 @@ create table music (
 );      
           
          
-create table movie (
-    movie_id SERIAL PRIMARY KEY,
-    person_id UUID NOT NULL,
-    movie_image VARCHAR NOT NULL,
-    movie_key VARCHAR NOT NULL,
-    movie_title VARCHAR NOT NULL,
-    movie_runtime VARCHAR NOT NULL,
-    movie_summary VARCHAR NOT NULL,
-    movie_release VARCHAR NOT NULL,
-    movie_genre VARCHAR NOT NULL,
-    movie_rating NUMERIC,
-    movie_trailer VARCHAR NOT NULL,
-    id_uid UUID REFERENCES person
-);
-
-create table book (
-  book_id SERIAL PRIMARY KEY,
-  person_id UUID NOT NULL,
-  book_image VARCHAR NOT NULL,
-  book_key BIGINT NOT NULL,
-  book_title VARCHAR NOT NULL,
-  book_author VARCHAR NOT NULL,
-  book_price NUMERIC NOT NULL,
-  book_currencyCode VARCHAR NOT NULL,
-  book_pages BIGINT NOT NULL,
-  id_uid UUID REFERENCES person
-);
 
 create table person (
     id_uid UUID PRIMARY KEY NOT NULL,
